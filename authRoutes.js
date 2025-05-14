512faeaea4a912f817946891e1622ee22893856c
Timi4tech <timileyinkusimo@gmail.com>
2025-05-14 15:38:10 +0100

    Update authRoutes.js

--- authRoutes.js
+++ authRoutes.js
@@ -1,14 +1,14 @@
 // server/routes/authRoutes.js
 
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/authController');
+const { registerUser, loginUser } = require('../authController.js');
 
 const router = express.Router();
 
 // @route POST /api/auth/register
-router.post('/register', registerUser);
+router.post('/register.js', registerUser);
 
 // @route POST /api/auth/login
-router.post('/login', loginUser);
+router.post('/login.js', loginUser);
 
 module.exports = router;
